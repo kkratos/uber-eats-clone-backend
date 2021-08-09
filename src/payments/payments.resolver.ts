7b9c4cd523c2b96f088792c2969fdaa2ee0bf3df
Kamal Patel <kamal.patel30@hotmail.com>
2021-08-09 14:07:06 +0530

    createPayment mutation in PaymentsResolver

---
+++ src/payments/payments.resolver.ts
@@ -0,0 +1,22 @@
+import { Args, Mutation, Resolver } from "@nestjs/graphql";
+import { AuthUser } from "src/auth/auth-user.decorator";
+import { Role } from "src/auth/role.decorator";
+import { User } from "src/users/entities/user.entity";
+import { CreatePaymentInput, CreatePaymentOuput } from "./dtos/create-payment.dto";
+import { Payment } from "./entities/payment.entity";
+import { PaymentsService } from "./payments.service";
+
+
+@Resolver(of => Payment)
+export class PaymentsResolver {
+    constructor(
+        private readonly paymentsService: PaymentsService
+    ) { }
+
+    @Mutation(returns => CreatePaymentOuput)
+    @Role(["Owner"])
+    createPayment(@AuthUser() owner: User, @Args("input") createPaymentInput: CreatePaymentInput): Promise<CreatePaymentOuput> {
+        return this.paymentsService.createPayment(owner, createPaymentInput)
+    }
+
+}
